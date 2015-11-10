0ab0aa94db7f4983bb7dbbc4ea82b669330ecb1e
young <young@localhost.localdomain>
2015-11-10 09:59:31 -0800

    episode controller and view 페이지 생성

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'welcome#index'
 
-   resources :podcasts, only: [:index, :show]
+   resources :podcasts, only: [:index, :show] do
+	resources :episodes
+   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
