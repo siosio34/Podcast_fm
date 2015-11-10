2f918192721ef3b3fa81b393676a26916e0f9d8e
young <young@localhost.localdomain>
2015-11-10 12:35:39 -0800

    수정, 삭제기능구현 , 인터페이스 수정 ,대쉬보드기능구현

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,9 +1,10 @@
 class EpisodesController < ApplicationController
 	before_action :find_podcast
-	before_action :find_episode, only: [:show]
+	before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@episode = @podcast.episodes.new
+
 	end
 	
 	def create
@@ -15,11 +16,29 @@ class EpisodesController < ApplicationController
 		end	
 	end
 
-	def 	show
+	def  show
 		@episodes = Episode.where(podcast_id:@podcast).order("created_at DESC").reject { |e| e.id == @episode.id }
 
 	end	
 
+	def edit
+
+	end
+	
+	def update
+		if @episode.update episode_params
+			redirect_to podcast_episode_path(@podcast, @episode), notice: "Episode was succesfully updated!"	
+
+		else
+			render 'edit'
+		end	
+	end
+
+	def destroy
+		@episode.destroy
+		redirect_to root_path
+	end	
+			
 	private
 
 	def episode_params
